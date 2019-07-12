3d391b1621d0eb2bb66be04c925cfc7cb2886b03
Charles Hathaway <chathaway@logrit.com>
2019-07-12 13:46:13 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	pdflatex main.tex
+clean:
+	rm -f *.aux *.log *.synctex.gz *.pdf
+
---
+++ figures/intro_names_schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE names (id INTEGER PRIMARY KEY
+	, firstName VARCHAR(30)
+	, lastName VARCHAR(30)
+) GLOBAL "^names(keys(""id""))";
